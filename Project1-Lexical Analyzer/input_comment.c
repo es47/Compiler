c88d6b86c716cbe9d627743386754efc4c4e81fa
Lchs <kl8534ghj4633@gmail.com>
2018-08-24 17:21:38 +0800

    Create input_comment.c

---
+++ Project1-Lexical Analyzer/input_comment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+		printf("Hello World~~\n"); //test test test!!!!&&&
+
+		printf("HELLO
+				WORLD
+				\n");
+
+		/* testing testing 9dj34m,wpcjv90******md3-2-22
+		jfi09e-[henkd******fie9-wuj3//\\\dk9e0
+		*************/
+
+		return 0;
+}
